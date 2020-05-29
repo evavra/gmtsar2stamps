02e1f5e17bcf5dc2ba4c4011aa534e031605e714
Ellis Vavra <ellisvavra@berkeley.edu>
2020-05-29 09:07:17 -0700

    python controller for making master-referenced interferograms for StaMPS

---
+++ master_intfs.py
@@ -0,0 +1,41 @@
+import sys
+import glob
+import subprocess
+
+
+def main():
+    """
+    Make all possible interferograms with master scene.
+    Run in slc directory and specify master.PRM at command line.
+    Example: master_intfs.py S1_20170905_ALL_F2.SLC
+    """
+
+    # First, check for topo_ra.grd
+    if len(glob.glob('topo_ra.grd')) < 1:
+        print('topo_ra.grd needs to be saved or linked in current directory!')
+        return
+
+    # Get list of PRMs
+    prms = glob.glob('*PRM')
+    prms.sort()
+
+    # Assign master from arguments
+    supermaster = sys.argv[1]
+
+    # Get index of supermaster in list
+    m = prms.index(supermaster)
+
+    # Loop through all
+    for i, PRM in enumerate(prms):
+        if i < m:
+            print('intf.csh {} {} -topo topo_ra.grd'.format(PRM, supermaster))
+            # subprocess.call('intf.csh {} {} -topo topo_ra.grd'.format(PRM, supermaster), shell=True)
+        elif i == m:
+            continue
+        elif i > m:
+            print('intf.csh {} {} -topo_ra.grd'.format(supermaster, PRM))
+            # subprocess.call('intf.csh {} {} -topo_ra.grd'.format(supermaster, PRM), shell=True)
+
+
+if __name__ == '__main__':
+    main()
